b00f127b1fe8a9effb322fadb323d1d3d1c85de7
prusa-dev <prusa@mail.ru>
2023-05-07 22:12:38 +0300

    use pwsh

--- connect.cmd
+++ connect.cmd
@@ -1,5 +1,5 @@
 @echo off
 
-powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -Command "cd '%~dp0%scripts'; & ./main.ps1"
+pwsh.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -Command "cd '%~dp0%scripts'; & ./main.ps1"
 
 if ERRORLEVEL 1 pause
