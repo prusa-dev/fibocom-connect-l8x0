96ecb8dd53a9e83bf16bd11f769635d9a959a07e
prusa-dev <prusa@mail.ru>
2023-05-22 12:36:13 +0300

    update to powershell.exe

--- monitor.cmd
+++ monitor.cmd
@@ -1,5 +1,5 @@
 @echo off
 
-pwsh.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -Command "cd '%~dp0%scripts'; & ./main.ps1 -OnlyMonitor"
+powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -Command "cd '%~dp0%scripts'; & ./main.ps1 -OnlyMonitor"
 
 if ERRORLEVEL 1 pause
