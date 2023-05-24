206380105880c08c19e5ac4fd6e61221d73edbc9
prusa-dev <prusa@mail.ru>
2023-05-24 11:45:46 +0300

    update pack script

--- tools/pack.ps1
+++ tools/pack.ps1
@@ -4,4 +4,8 @@ $git_hash = $(git rev-parse --short HEAD)
 
 Compress-Archive -Force -CompressionLevel Optimal `
     -DestinationPath "./dist/fibocom-connect.$git_hash.zip" `
-    -Path './*.cmd', './scripts', './drivers', './screenshot', './*.md'
+    -Path './*.cmd', './scripts', './screenshot', './*.md'
+
+Compress-Archive -Force -CompressionLevel Optimal `
+    -DestinationPath "./dist/drivers_l860.zip" `
+    -Path './drivers'
