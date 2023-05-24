1bb259c5490d2e1704aaafde2c9455812638c280
prusa-dev <prusa@mail.ru>
2023-05-24 11:46:57 +0300

    update pack script

--- tools/pack.ps1
+++ tools/pack.ps1
@@ -1,6 +1,6 @@
 mkdir -Force ./dist | Out-Null
 
-$git_hash = $(git rev-parse --short HEAD)
+$git_hash = $(git describe --tags)
 
 Compress-Archive -Force -CompressionLevel Optimal `
     -DestinationPath "./dist/fibocom-connect.$git_hash.zip" `
