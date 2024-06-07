28ac6edc5ee7be147cc6cc7920c161ca7d64e389
prusa-dev <prusa@mail.ru>
2024-06-07 18:35:05 +0300

    update main loop

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -48,8 +48,15 @@ Import-Module ./modules/serial-port.psm1
 Import-Module ./modules/converters.psm1
 Import-Module ./modules/network.psm1
 
-try {
-    while ($true) {
+$defaultCursorSize = $Host.UI.RawUI.CursorSize;
+
+$modem = $null
+
+### Hide cursor
+$Host.UI.RawUI.CursorSize = 0
+
+while ($true) {
+    try {
         Clear-Host
 
         Write-Host "=== $app_version ==="
@@ -239,167 +246,166 @@ try {
         ### Monitoring
         Write-Host
         Write-Host "=== Status ==="
-        $cursorSize = $Host.UI.RawUI.CursorSize; $Host.UI.RawUI.CursorSize = 0
-        try {
-            $statusCursorPosition = $Host.UI.RawUI.CursorPosition
 
-            while ($true) {
-                if ((Get-Event -SourceIdentifier $watchdogEventSource -ErrorAction SilentlyContinue)) {
-                    break
-                }
+        $Host.UI.RawUI.CursorSize = 0
+        $statusCursorPosition = $Host.UI.RawUI.CursorPosition
 
-                $response = ''
-
-                # Temperature doesn't work on L850
-                if (-Not($model -match 'L850')) {
-                    $response += Send-ATCommand -Port $modem -Command "AT+MTSM=1"
-                }
-                $response += Send-ATCommand -Port $modem -Command "AT+COPS?"
-                $response += Send-ATCommand -Port $modem -Command "AT+CSQ?"
-                $response += Send-ATCommand -Port $modem -Command "AT+XCCINFO?; +XLEC?; +XMCI=1"
-
-                if ([string]::IsNullOrEmpty($response)) {
-                    continue
-                }
-
-                [nullable[int]]$tech = $response | Awk -Split '(?<=\+COPS):|,' -Filter '\+COPS:' -Action { $args[4] }
-                $mode = switch ($tech) {
-                    0 { 'EDGE' }
-                    2 { 'UMTS' }
-                    3 { 'LTE' }
-                    4 { 'HSDPA' }
-                    5 { 'HSUPA' }
-                    6 { 'HSPA' }
-                    7 { 'LTE' }
-                    default { $null }
-                }
-
-                $oper = $response | Awk -Split '(?<=\+COPS):|,' -Filter '\+COPS:' -Action { $args[3] -replace '"', '' }
-
-                [nullable[int]]$temp = $response | Awk -Split '[:,]' -Filter '\+MTSM:' -Action { $args[1] }
-
-                $csq = $response | Awk -Split '[:,]' -Filter '\+CSQ:' -Action { [int]$args[1] }
-                $csq_perc = 0
-                if ($csq -ge 0 -and $csq -le 31) {
-                    $csq_perc = $csq * 100 / 31
-                }
-                #$cqs_rssi = 2 * $csq - 113
-
-                [nullable[int]]$u_dluarfnc = $response | Awk -Split '[:,]' -Filter '\+XMCI: 2' -Action { $args[7] -replace '"', '' }
-                [nullable[double]]$u_rssi = $response | Awk -Split '[:,]' -Filter '\+XMCI: 2' -Action { [int]$args[10] - 111 }
-                [nullable[double]]$u_rscp = $response | Awk -Split '[:,]' -Filter '\+XMCI: 2' -Action { [int]$args[11] - 121 }
-                [nullable[double]]$u_ecno = $response | Awk -Split '[:,]' -Filter '\+XMCI: 2' -Action { ([int]$args[12] / 2) - 24 }
-
-                [nullable[int]]$dluarfnc = $response | Awk -Split '[:,]' -Filter '\+XMCI: 4' -Action { $args[7] -replace '"', '' }
-                [nullable[double]]$rsrp = $response | Awk -Split '[:,]' -Filter '\+XMCI: 4' -Action { ([int]$args[10]) - 141 }
-                [nullable[double]]$rsrq = $response | Awk -Split '[:,]' -Filter '\+XMCI: 4' -Action { ([int]$args[11]) / 2 - 20 }
-                [nullable[double]]$sinr = $response | Awk -Split '[:,]' -Filter '\+XMCI: 4' -Action { ([int]$args[12]) / 2 }
-                [nullable[int]]$ta = $response | Awk -Split '[:,]' -Filter '\+XMCI: 4' -Action { $args[13] -replace '"', '' }
-                $distance = Invoke-NullCoalescing { [Math]::Round(($ta * 78.125) / 1000, 3) } { $null }
-
-                [nullable[int]]$bw = $response | Awk -Split '[:,]' -Filter '\+XLEC:' -Action { [int]$args[3] }
-
-                $rssi = Convert-RsrpToRssi $rsrp $bw
-
-
-                [int[]]$ci_x = $response | Awk -Split '[:,]' -Filter '\+XMCI: [45]' -Action { [int]($args[5] -replace '"', '') }
-                [int[]]$pci_x = $response | Awk -Split '[:,]' -Filter '\+XMCI: [45]' -Action { [int]($args[6] -replace '"', '') }
-                [int[]]$dluarfnc_x = $response | Awk -Split '[:,]' -Filter '\+XMCI: [45]' -Action { [int]($args[7] -replace '"', '') }
-                [string[]]$band_x = $dluarfnc_x | Get-BandLte
-                [int[]]$rsrp_x = $response | Awk -Split '[:,]' -Filter '\+XMCI: [45]' -Action { ([int]$args[10]) - 141 }
-                [int[]]$rsrq_x = $response | Awk -Split '[:,]' -Filter '\+XMCI: [45]' -Action { ([int]$args[11]) / 2 - 20 }
-
-                $band = '--'
-                $ca_match = [regex]::Match($response, "\+XLEC: (?:\d+),(?<no_of_cells>\d+),(?:(?<bw>\d+),*)+(?:BAND_LTE_(?:(?<band>\d+),*)+)?")
-                if ($ca_match.Success) {
-                    $ca_number = $ca_match.Groups['no_of_cells'].Value
-
-                    [int[]]$ca_bw_x = $ca_match.Groups['bw'].Captures | ForEach-Object { [int]$_.Value }
-                    [string[]]$ca_band_x = $ca_match.Groups['band'].Captures | Where-Object { $_.Value -gt 0 } | ForEach-Object { "B$_" }
-
-                    if ($ca_band_x.Length -ne $ca_number) {
-                        $ca_band_x = $band_x
-                    }
-
-                    $band = ''
-                    for (($i = 0); $i -lt $ca_number; $i++) {
-                        $band += "{0}@{1}MHz " -f $ca_band_x[$i], (Get-BandwidthFrequency $ca_bw_x[$i])
-                    }
-                }
-                elseif ($null -ne $dluarfnc) {
-                    $band = "{0}@{1}MHz" -f (Get-BandLte $dluarfnc), (Get-BandwidthFrequency $bw)
-                }
-
-                ### Display
-                $Host.UI.RawUI.CursorPosition = $statusCursorPosition
-
-                $lineWidth = $Host.UI.RawUI.BufferSize.Width
-                $titleWidth = 17
-
-                if ($null -ne $temp) {
-                    Write-Host ("{0,-$lineWidth}" -f ("{0,-$titleWidth} {1,4:f0} $([char]0xB0)C" -f "Temp:", $temp))
-                }
-                Write-Host ("{0,-$lineWidth}" -f ("{0,-$titleWidth} {1} ({2})" -f "Operator:", (Invoke-NullCoalescing $oper '----'), (Invoke-NullCoalescing $mode '--')))
-
-                if ($null -ne $mode) {
-                    Write-Host ("{0,-$lineWidth}" -f ("{0,-$titleWidth} {1}km" -f "Distance:", (Invoke-NullCoalescing $distance '--')))
-                    Write-Host ("{0,-$lineWidth}" -f ("{0,-$titleWidth} {1,4:f0}%   {2}" -f "Signal:", $csq_perc, (Get-Bars -Value $csq_perc -Min 0 -Max 100)))
-                }
-
-                if ($mode -eq 'UMTS') {
-                    Write-Host ("{0,-$lineWidth}" -f ("{0,-$titleWidth} {1,4:f0}dBm {2}" -f "RSSI:", $u_rssi, (Get-Bars -Value $u_rssi -Min -120 -Max -25)))
-                    Write-Host ("{0,-$lineWidth}" -f ("{0,-$titleWidth} {1,4:f0}dBm {2}" -f "RSCP:", $u_rscp, (Get-Bars -Value $u_rscp -Min -120 -Max -25)))
-                    Write-Host ("{0,-$lineWidth}" -f ("{0,-$titleWidth} {1,4:f0}dB  {2}" -f "ECNO:", $u_ecno, (Get-Bars -Value $u_ecno -Min -24 -Max 0)))
-                    Write-Host ("{0,-$lineWidth}" -f ("{0,-$titleWidth} {1}" -f "UARFCN:", $u_dluarfnc))
-                }
-                elseif ($mode -eq 'LTE') {
-
-                    Write-Host ("{0,-$lineWidth}" -f ("{0,-$titleWidth} {1,4:f0}dBm {2}" -f "RSSI:", $rssi, (Get-Bars -Value $rssi -Min -110 -Max -25)))
-                    Write-Host ("{0,-$lineWidth}" -f ("{0,-$titleWidth} {1,4:f0}dB  {2}" -f "SINR:", $sinr, (Get-Bars -Value $sinr -Min -10 -Max 30)))
-                    Write-Host ("{0,-$lineWidth}" -f ("{0,-$titleWidth} {1,4:f0}dBm {2}" -f "RSRP:", $rsrp, (Get-Bars -Value $rsrp -Min -120 -Max -50)))
-                    Write-Host ("{0,-$lineWidth}" -f ("{0,-$titleWidth} {1,4:f0}dB  {2}" -f "RSRQ:", $rsrq, (Get-Bars -Value $rsrq -Min -25 -Max -1)))
-
-                    Write-Host ("{0,-$lineWidth}" -f ("{0,-$titleWidth} {1}" -f "Band:", $band))
-                    Write-Host ("{0,-$lineWidth}" -f ("{0,-$titleWidth} {1}" -f "EARFCN:", (Invoke-NullCoalescing $dluarfnc '--')))
-
-                    $carriers_count = $pci_x.Length
-                    for (($i = 0); $i -lt $carriers_count; $i++) {
-                        Write-Host -NoNewline ("===Carrier {0,2}: " -f ($i + 1))
-                        Write-Host -NoNewline ("{0} {1,9} " -f "CI:", $ci_x[$i])
-                        Write-Host -NoNewline ("{0} {1,5} " -f "PCI:", $pci_x[$i])
-                        Write-Host -NoNewline ("{0} {1,3} ({2,5}) " -f "Band (EARFCN):", $band_x[$i], $dluarfnc_x[$i])
-                        Write-Host -NoNewline ("{0} {1,4:f0}dBm {2} " -f "RSRP:", $rsrp_x[$i], (Get-Bars -Value $rsrp_x[$i] -Min -120 -Max -50))
-                        Write-Host -NoNewline ("{0} {1,4:f0}dB  {2} " -f "RSRQ:", $rsrq_x[$i], (Get-Bars -Value $rsrq_x[$i] -Min -25 -Max -1))
-                        Write-Host
-                    }
-                }
-
-                ### Clear
-                $lastCusrsorPosition = $Host.UI.RawUI.CursorPosition
-                $cleanBuffer = $Host.UI.RawUI.NewBufferCellArray(
-                    @{ Width = $Host.UI.RawUI.BufferSize.Width; Height = 200 },
-                    @{ Character = ' '; ForegroundColor = $Host.UI.RawUI.ForegroundColor; BackgroundColor = $Host.UI.RawUI.BackgroundColor } )
-                $Host.UI.RawUI.SetBufferContents($lastCusrsorPosition, $cleanBuffer)
-
-                Start-Sleep -Seconds 2
+        while ($true) {
+            if ((Get-Event -SourceIdentifier $watchdogEventSource -ErrorAction SilentlyContinue)) {
+                break
             }
-        }
-        catch {
-            Write-Error2 $_
-            Start-Sleep -Seconds 5
-        }
-        finally {
-            $Host.UI.RawUI.CursorSize = $cursorSize
-        }
 
+            $response = ''
+
+            # Temperature doesn't work on L850
+            if (-Not($model -match 'L850')) {
+                $response += Send-ATCommand -Port $modem -Command "AT+MTSM=1"
+            }
+            $response += Send-ATCommand -Port $modem -Command "AT+COPS?"
+            $response += Send-ATCommand -Port $modem -Command "AT+CSQ?"
+            $response += Send-ATCommand -Port $modem -Command "AT+XCCINFO?; +XLEC?; +XMCI=1"
+
+            if ([string]::IsNullOrEmpty($response)) {
+                continue
+            }
+
+            [nullable[int]]$tech = $response | Awk -Split '(?<=\+COPS):|,' -Filter '\+COPS:' -Action { $args[4] }
+            $mode = switch ($tech) {
+                0 { 'EDGE' }
+                2 { 'UMTS' }
+                3 { 'LTE' }
+                4 { 'HSDPA' }
+                5 { 'HSUPA' }
+                6 { 'HSPA' }
+                7 { 'LTE' }
+                default { $null }
+            }
+
+            $oper = $response | Awk -Split '(?<=\+COPS):|,' -Filter '\+COPS:' -Action { $args[3] -replace '"', '' }
+
+            [nullable[int]]$temp = $response | Awk -Split '[:,]' -Filter '\+MTSM:' -Action { $args[1] }
+
+            $csq = $response | Awk -Split '[:,]' -Filter '\+CSQ:' -Action { [int]$args[1] }
+            $csq_perc = 0
+            if ($csq -ge 0 -and $csq -le 31) {
+                $csq_perc = $csq * 100 / 31
+            }
+            #$cqs_rssi = 2 * $csq - 113
+
+            [nullable[int]]$u_dluarfnc = $response | Awk -Split '[:,]' -Filter '\+XMCI: 2' -Action { $args[7] -replace '"', '' }
+            [nullable[double]]$u_rssi = $response | Awk -Split '[:,]' -Filter '\+XMCI: 2' -Action { [int]$args[10] - 111 }
+            [nullable[double]]$u_rscp = $response | Awk -Split '[:,]' -Filter '\+XMCI: 2' -Action { [int]$args[11] - 121 }
+            [nullable[double]]$u_ecno = $response | Awk -Split '[:,]' -Filter '\+XMCI: 2' -Action { ([int]$args[12] / 2) - 24 }
+
+            [nullable[int]]$dluarfnc = $response | Awk -Split '[:,]' -Filter '\+XMCI: 4' -Action { $args[7] -replace '"', '' }
+            [nullable[double]]$rsrp = $response | Awk -Split '[:,]' -Filter '\+XMCI: 4' -Action { ([int]$args[10]) - 141 }
+            [nullable[double]]$rsrq = $response | Awk -Split '[:,]' -Filter '\+XMCI: 4' -Action { ([int]$args[11]) / 2 - 20 }
+            [nullable[double]]$sinr = $response | Awk -Split '[:,]' -Filter '\+XMCI: 4' -Action { ([int]$args[12]) / 2 }
+            [nullable[int]]$ta = $response | Awk -Split '[:,]' -Filter '\+XMCI: 4' -Action { $args[13] -replace '"', '' }
+            $distance = Invoke-NullCoalescing { [Math]::Round(($ta * 78.125) / 1000, 3) } { $null }
+
+            [nullable[int]]$bw = $response | Awk -Split '[:,]' -Filter '\+XLEC:' -Action { [int]$args[3] }
+
+            $rssi = Convert-RsrpToRssi $rsrp $bw
+
+
+            [int[]]$ci_x = $response | Awk -Split '[:,]' -Filter '\+XMCI: [45]' -Action { [int]($args[5] -replace '"', '') }
+            [int[]]$pci_x = $response | Awk -Split '[:,]' -Filter '\+XMCI: [45]' -Action { [int]($args[6] -replace '"', '') }
+            [int[]]$dluarfnc_x = $response | Awk -Split '[:,]' -Filter '\+XMCI: [45]' -Action { [int]($args[7] -replace '"', '') }
+            [string[]]$band_x = $dluarfnc_x | Get-BandLte
+            [int[]]$rsrp_x = $response | Awk -Split '[:,]' -Filter '\+XMCI: [45]' -Action { ([int]$args[10]) - 141 }
+            [int[]]$rsrq_x = $response | Awk -Split '[:,]' -Filter '\+XMCI: [45]' -Action { ([int]$args[11]) / 2 - 20 }
+
+            $band = '--'
+            $ca_match = [regex]::Match($response, "\+XLEC: (?:\d+),(?<no_of_cells>\d+),(?:(?<bw>\d+),*)+(?:BAND_LTE_(?:(?<band>\d+),*)+)?")
+            if ($ca_match.Success) {
+                $ca_number = $ca_match.Groups['no_of_cells'].Value
+
+                [int[]]$ca_bw_x = $ca_match.Groups['bw'].Captures | ForEach-Object { [int]$_.Value }
+                [string[]]$ca_band_x = $ca_match.Groups['band'].Captures | Where-Object { $_.Value -gt 0 } | ForEach-Object { "B$_" }
+
+                if ($ca_band_x.Length -ne $ca_number) {
+                    $ca_band_x = $band_x
+                }
+
+                $band = ''
+                for (($i = 0); $i -lt $ca_number; $i++) {
+                    $band += "{0}@{1}MHz " -f $ca_band_x[$i], (Get-BandwidthFrequency $ca_bw_x[$i])
+                }
+            }
+            elseif ($null -ne $dluarfnc) {
+                $band = "{0}@{1}MHz" -f (Get-BandLte $dluarfnc), (Get-BandwidthFrequency $bw)
+            }
+
+            ### Display
+            $Host.UI.RawUI.CursorPosition = $statusCursorPosition
+
+            $lineWidth = $Host.UI.RawUI.BufferSize.Width
+            $titleWidth = 17
+
+            if ($null -ne $temp) {
+                Write-Host ("{0,-$lineWidth}" -f ("{0,-$titleWidth} {1,4:f0} $([char]0xB0)C" -f "Temp:", $temp))
+            }
+            Write-Host ("{0,-$lineWidth}" -f ("{0,-$titleWidth} {1} ({2})" -f "Operator:", (Invoke-NullCoalescing $oper '----'), (Invoke-NullCoalescing $mode '--')))
+
+            if ($null -ne $mode) {
+                Write-Host ("{0,-$lineWidth}" -f ("{0,-$titleWidth} {1}km" -f "Distance:", (Invoke-NullCoalescing $distance '--')))
+                Write-Host ("{0,-$lineWidth}" -f ("{0,-$titleWidth} {1,4:f0}%   {2}" -f "Signal:", $csq_perc, (Get-Bars -Value $csq_perc -Min 0 -Max 100)))
+            }
+
+            if ($mode -eq 'UMTS') {
+                Write-Host ("{0,-$lineWidth}" -f ("{0,-$titleWidth} {1,4:f0}dBm {2}" -f "RSSI:", $u_rssi, (Get-Bars -Value $u_rssi -Min -120 -Max -25)))
+                Write-Host ("{0,-$lineWidth}" -f ("{0,-$titleWidth} {1,4:f0}dBm {2}" -f "RSCP:", $u_rscp, (Get-Bars -Value $u_rscp -Min -120 -Max -25)))
+                Write-Host ("{0,-$lineWidth}" -f ("{0,-$titleWidth} {1,4:f0}dB  {2}" -f "ECNO:", $u_ecno, (Get-Bars -Value $u_ecno -Min -24 -Max 0)))
+                Write-Host ("{0,-$lineWidth}" -f ("{0,-$titleWidth} {1}" -f "UARFCN:", $u_dluarfnc))
+            }
+            elseif ($mode -eq 'LTE') {
+
+                Write-Host ("{0,-$lineWidth}" -f ("{0,-$titleWidth} {1,4:f0}dBm {2}" -f "RSSI:", $rssi, (Get-Bars -Value $rssi -Min -110 -Max -25)))
+                Write-Host ("{0,-$lineWidth}" -f ("{0,-$titleWidth} {1,4:f0}dB  {2}" -f "SINR:", $sinr, (Get-Bars -Value $sinr -Min -10 -Max 30)))
+                Write-Host ("{0,-$lineWidth}" -f ("{0,-$titleWidth} {1,4:f0}dBm {2}" -f "RSRP:", $rsrp, (Get-Bars -Value $rsrp -Min -120 -Max -50)))
+                Write-Host ("{0,-$lineWidth}" -f ("{0,-$titleWidth} {1,4:f0}dB  {2}" -f "RSRQ:", $rsrq, (Get-Bars -Value $rsrq -Min -25 -Max -1)))
+
+                Write-Host ("{0,-$lineWidth}" -f ("{0,-$titleWidth} {1}" -f "Band:", $band))
+                Write-Host ("{0,-$lineWidth}" -f ("{0,-$titleWidth} {1}" -f "EARFCN:", (Invoke-NullCoalescing $dluarfnc '--')))
+
+                $carriers_count = $pci_x.Length
+                for (($i = 0); $i -lt $carriers_count; $i++) {
+                    Write-Host -NoNewline ("===Carrier {0,2}: " -f ($i + 1))
+                    Write-Host -NoNewline ("{0} {1,9} " -f "CI:", $ci_x[$i])
+                    Write-Host -NoNewline ("{0} {1,5} " -f "PCI:", $pci_x[$i])
+                    Write-Host -NoNewline ("{0} {1,3} ({2,5}) " -f "Band (EARFCN):", $band_x[$i], $dluarfnc_x[$i])
+                    Write-Host -NoNewline ("{0} {1,4:f0}dBm {2} " -f "RSRP:", $rsrp_x[$i], (Get-Bars -Value $rsrp_x[$i] -Min -120 -Max -50))
+                    Write-Host -NoNewline ("{0} {1,4:f0}dB  {2} " -f "RSRQ:", $rsrq_x[$i], (Get-Bars -Value $rsrq_x[$i] -Min -25 -Max -1))
+                    Write-Host
+                }
+            }
+
+            ### Clear
+            $lastCusrsorPosition = $Host.UI.RawUI.CursorPosition
+            $cleanBuffer = $Host.UI.RawUI.NewBufferCellArray(
+                @{ Width = $Host.UI.RawUI.BufferSize.Width; Height = 200 },
+                @{ Character = ' '; ForegroundColor = $Host.UI.RawUI.ForegroundColor; BackgroundColor = $Host.UI.RawUI.BackgroundColor } )
+            $Host.UI.RawUI.SetBufferContents($lastCusrsorPosition, $cleanBuffer)
+
+            Start-Sleep -Seconds 2
+        }
+    }
+    catch {
+        Write-Error2 "`n$_ `n$($_.FullyQualifiedErrorId) `n $($_.ScriptStackTrace)"
+        Write-Verbose "`n$_ `n$($_.FullyQualifiedErrorId) `n $($_.ScriptStackTrace)"
+    }
+    finally {
+        $Host.UI.RawUI.CursorSize = $defaultCursorSize
         Stop-NetworkMonitoring
         Stop-SerialPortMonitoring
         Get-Event -SourceIdentifier $watchdogEventSource -ErrorAction SilentlyContinue | Remove-Event
-        Close-SerialPort -Port $modem
+        if ($modem) {
+            Close-SerialPort -Port $modem
+            $modem.Dispose()
+            $modem = $null
+        }
     }
-}
-finally {
-    Stop-NetworkMonitoring
-    Stop-SerialPortMonitoring
-    Close-SerialPort -Port $modem
+
+    Start-Sleep -Seconds 5
 }
